9f56ec13681b2c9136fd5fba84337b6f3cb40227
Pappu Kumar Singh <pappuksingh1008@gmail.com>
2024-09-07 17:47:07 +0400

    Update AdvertiserDeleteForm.php

--- AdvertiserDeleteForm.php
+++ AdvertiserDeleteForm.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Drupal\example_module\Form;
+namespace Drupal\drupal10_custom_module\Form;
 
 use Drupal\Core\Entity\ContentEntityConfirmFormBase;
 use Drupal\Core\Form\FormStateInterface;
